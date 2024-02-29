eb985d1a7c1676885e93c1eae0b061b5bcea1e1b
Eclipse <eclipse@eclipseisoffline.xyz>
2024-02-29 15:18:45 +0000

    Optimised code, version bump, update fabric.mod.json

--- src/main/java/xyz/eclipseisoffline/jukeboxcustomdiscfix/mixin/JukeboxBlockEntityMixin.java
+++ src/main/java/xyz/eclipseisoffline/jukeboxcustomdiscfix/mixin/JukeboxBlockEntityMixin.java
@@ -5,33 +5,20 @@ import net.minecraft.world.item.ItemStack;
 import net.minecraft.world.level.Level;
 import net.minecraft.world.level.block.entity.JukeboxBlockEntity;
 import org.spongepowered.asm.mixin.Mixin;
-import org.spongepowered.asm.mixin.Unique;
+import org.spongepowered.asm.mixin.Shadow;
 import org.spongepowered.asm.mixin.injection.At;
-import org.spongepowered.asm.mixin.injection.Inject;
 import org.spongepowered.asm.mixin.injection.Redirect;
-import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
-import org.spongepowered.asm.mixin.injection.callback.CallbackInfoReturnable;
 
 @Mixin(JukeboxBlockEntity.class)
 public abstract class JukeboxBlockEntityMixin {
 
-    @Unique
-    private boolean manuallyStopped = false;
+    @Shadow
+    public abstract ItemStack getTheItem();
 
     @Redirect(method = "stopPlaying", at = @At(value = "INVOKE", target = "Lnet/minecraft/world/level/Level;levelEvent(ILnet/minecraft/core/BlockPos;I)V"))
     public void cancelStopRecord(Level instance, int type, BlockPos blockPos, int data) {
-        if (manuallyStopped) {
+        if (getTheItem().isEmpty()) {
             instance.levelEvent(type, blockPos, data);
         }
-    }
-
-    @Inject(method = "startPlaying", at = @At("HEAD"))
-    public void unsetManuallyStopped(CallbackInfo callbackInfo) {
-        manuallyStopped = false;
-    }
-
-    @Inject(method = "splitTheItem", at = @At(value = "INVOKE", target = "Lnet/minecraft/world/level/block/entity/JukeboxBlockEntity;stopPlaying()V"))
-    public void setManuallyStopped(int amount, CallbackInfoReturnable<ItemStack> callbackInfoReturnable) {
-        manuallyStopped = true;
     }
 }
