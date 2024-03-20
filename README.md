66f4564808eb14c70c30eafe8887924403453672
Eclipse <eclipse@eclipseisoffline.xyz>
2024-03-20 18:04:39 +0000

    Made small changes to implementation, version bump and update README

--- README.md
+++ README.md
@@ -3,13 +3,13 @@
 This mod fixes bug [MC-260346](https://bugs.mojang.com/browse/MC-260346), which causes custom music
 discs, when played in a jukebox, to be cut off when the vanilla disc would normally end.
 
-The fix is implemented by only telling the client to stop playing a disc sound when the disc item is removed 
+The fix is implemented server-side by only telling the client to stop playing a disc sound when the disc item is removed 
 from the jukebox (either by a hopper or by a player clicking on the jukebox), whereas in vanilla
 Minecraft the client is told to stop playing a disc sound as soon as the vanilla disc would end.
 
-This mod does not have to be installed client side.
+This mod does not have to be installed client side when playing multiplayer.
 
-Feel free to report any bugs, or suggest new features, at the issue tracker.
+Feel free to report any bugs, at the issue tracker.
 
 ## License
 
@@ -17,7 +17,8 @@ This mod is licensed under GNU GPLv3.
 
 ## Usage
 
-Mod builds can be found [here]()https://github.com/eclipseisoffline/jukeboxcustomdiscfix/packages/2082474.
+Mod builds can be found [here](https://github.com/eclipseisoffline/jukeboxcustomdiscfix/packages/2082474)
+and on [Modrinth](https://modrinth.com/mod/jukebox-custom-disc-fix).
 
-This mod is currently available for Minecraft 1.20.4 (Fabric modloader 0.15.7 or higher).
-The Fabric API is not required. This mod is not required on clients.
+This mod is currently available for Minecraft 1.20.4 and Minecraft 1.20.1 (Fabric modloader 0.15.7 or higher).
+The Fabric API is not required. This mod is not required on clients when playing multiplayer.
--- src/main/java/xyz/eclipseisoffline/jukeboxcustomdiscfix/mixin/JukeboxBlockEntityMixin.java
+++ src/main/java/xyz/eclipseisoffline/jukeboxcustomdiscfix/mixin/JukeboxBlockEntityMixin.java
@@ -1,24 +1,36 @@
 package xyz.eclipseisoffline.jukeboxcustomdiscfix.mixin;
 
 import net.minecraft.core.BlockPos;
+import net.minecraft.world.Clearable;
 import net.minecraft.world.item.ItemStack;
-import net.minecraft.world.level.Level;
+import net.minecraft.world.level.block.entity.BlockEntity;
+import net.minecraft.world.level.block.entity.BlockEntityType;
 import net.minecraft.world.level.block.entity.JukeboxBlockEntity;
+import net.minecraft.world.level.block.state.BlockState;
+import net.minecraft.world.ticks.ContainerSingleItem;
+import org.jetbrains.annotations.NotNull;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.Shadow;
 import org.spongepowered.asm.mixin.injection.At;
-import org.spongepowered.asm.mixin.injection.Redirect;
+import org.spongepowered.asm.mixin.injection.Inject;
+import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
 
 @Mixin(JukeboxBlockEntity.class)
-public abstract class JukeboxBlockEntityMixin {
+public abstract class JukeboxBlockEntityMixin extends BlockEntity implements Clearable, ContainerSingleItem {
+
+    public JukeboxBlockEntityMixin(BlockEntityType<?> type,
+            BlockPos pos, BlockState blockState) {
+        super(type, pos, blockState);
+    }
 
     @Shadow
-    public abstract ItemStack getTheItem();
+    public abstract @NotNull ItemStack getTheItem();
 
-    @Redirect(method = "stopPlaying", at = @At(value = "INVOKE", target = "Lnet/minecraft/world/level/Level;levelEvent(ILnet/minecraft/core/BlockPos;I)V"))
-    public void cancelStopRecord(Level instance, int type, BlockPos blockPos, int data) {
-        if (getTheItem().isEmpty()) {
-            instance.levelEvent(type, blockPos, data);
+    @Inject(method = "stopPlaying", at = @At(value = "INVOKE", target = "Lnet/minecraft/world/level/Level;levelEvent(ILnet/minecraft/core/BlockPos;I)V"), cancellable = true)
+    public void cancelStopRecordWhenNotEmpty(CallbackInfo callbackInfo) {
+        if (!getTheItem().isEmpty()) {
+            setChanged();
+            callbackInfo.cancel();
         }
     }
 }
